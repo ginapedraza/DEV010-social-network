8245d984e5956ede6da53d34800478cfd518835f
ginapedraza <ginapedraza00@gmail.com>
2023-08-14 17:03:30 -0400

    instalacion firebase error firebase/app solucionado

--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+
 // Este es el punto de entrada de tu aplicacion
+import { initializeApp } from 'firebase/app';
 import { myFunction } from './lib/index.js';
 import home from './home.js';
 import login from './login.js';
 import error from './error.js';
+// Import the functions you need from the SDKs you need
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBZcFUeT__RAymtnLG7k1RsQLb8I1UlRUI',
+  authDomain: 'social-network-f2550.firebaseapp.com',
+  projectId: 'social-network-f2550',
+  storageBucket: 'social-network-f2550.appspot.com',
+  messagingSenderId: '259171374910',
+  appId: '1:259171374910:web:0c8a0b1b3999119268fe6f',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const routes = [
   { path: '/', component: home },
   { path: '/login', component: login },
